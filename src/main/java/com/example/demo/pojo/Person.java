735e58ee17827566f26fbbc4fde233fd9d1e513a
lihu <2840119419@qq.com>
2019-09-10 09:33:59 +0800

    springboot-jpa-一对一级联

---
+++ src/main/java/com/example/demo/pojo/Person.java
@@ -0,0 +1,79 @@
+package com.example.demo.pojo;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Person {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private Integer cardId;
+
+    @Id
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "age")
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Basic
+    @Column(name = "card_id")
+    public Integer getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(Integer cardId) {
+        this.cardId = cardId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (!Objects.equals(id, person.id)) return false;
+        if (!Objects.equals(name, person.name)) return false;
+        if (!Objects.equals(age, person.age)) return false;
+        if (!Objects.equals(cardId, person.cardId)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (cardId != null ? cardId.hashCode() : 0);
+        return result;
+    }
+}
